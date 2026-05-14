95666eb0ad6e540d70b1a1aa52f5aa7a6733a9da
JITENDRAKAR <66376714+JITENDRAKAR@users.noreply.github.com>
2026-05-14 20:48:47 +0530

    Switch email configuration to netprofit.i.t.s@gmail.com

--- investment_advisory/settings.py
+++ investment_advisory/settings.py
@@ -310,8 +310,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'foliuxits@gmail.com'
-EMAIL_HOST_PASSWORD = 'ohxu ntbh lyxw sukx'
+EMAIL_HOST_USER = 'netprofit.i.t.s@gmail.com'
+EMAIL_HOST_PASSWORD = 'vdhddqssiroxpfnx'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_SSL = False
 
